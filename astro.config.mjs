3eb1eb8f7426910ecbf7ee00988303286929acbe
Matt Levorson <matt@mlpvideo.com>
2026-07-03 18:11:50 -0500

    chore: integrate google analytics via partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import partytown from '@astrojs/partytown';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://aleaapts.com',
+  site: 'https://aleaapts.com',
+  integrations: [
+    partytown({
+      config: {
+        forward: ['dataLayer.push', 'gtag'],
+      },
+    }),
+  ],
 });
